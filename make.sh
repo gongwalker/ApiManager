70a1aa2d1cbc558aedfb545110466463a1ca720f
gognwen <bestphper@126.com>
2021-03-12 21:55:46 +0800

    添加交叉编译sh脚本

--- make.sh
+++ make.sh
@@ -1,8 +1,43 @@
+#!/bin/bash
+# @Author: gongwen
+# @Date:   2021-03-12 21:52:03
+# @Last Modified by:   gongwen
+# @Last Modified time: 2021-03-12 21:52:03
 
-# 建立release目录
+build() {
+  os=$1
+  echo "build ${os} being"
+  CGO_ENABLED=0 GOOS=${os} GOARCH=amd64 go build -o ApiManager-${os} main.go &&
+    mkdir -p releases/ApiManager-${os}-amd64/app &&
+    mv ApiManager-${os} releases/ApiManager-${os}-amd64/ApiManager &&
+    cp -rp db.sql config releases/ApiManager-${os}-amd64/ &&
+    cp -rp app/static app/views releases/ApiManager-${os}-amd64/app
 
-mkdir -p releases/ApiManager-version-linux-amd64
+  if [ $os = linux ] || [ $os = mac ]; then
+    cp -rp run.sh config releases/ApiManager-${os}-amd64/
+  fi
 
-
-//CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
-//CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build
+  echo "build ${os} end"
+}
+case $1 in
+build)
+  if [[ $2 = "linux" ]]; then
+    build linux
+  elif [[ $2 = "windows" ]]; then
+    build windows
+  elif [[ $2 = "darwin" ]]; then
+    build darwin
+  else
+      build linux
+      build windows
+      build darwin
+  fi
+  ;;
+clear)
+  rm -rf releases
+  ;;
+*)
+  echo "$0 build [linux|windows|darwin|all] | clear"
+  exit 4
+  ;;
+esac
